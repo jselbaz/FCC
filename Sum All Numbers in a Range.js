b9d0997eefeb652949ab22537f57322c2b406c50
Sefi <jselbaz@gmail.com>
2019-01-02 10:13:12 -0600

    Update Sum All Numbers in a Range.js

--- Sum All Numbers in a Range.js
+++ Sum All Numbers in a Range.js
@@ -1,3 +1,6 @@
+// We'll pass you an array of two numbers. Return the sum of those two numbers plus the sum of all the numbers between them.
+// The lowest number will not always come first.
+
 function sumAll(arr) {
   let max = Math.max(arr[0], arr[1]);
   let min = Math.min(arr[0], arr[1]);
